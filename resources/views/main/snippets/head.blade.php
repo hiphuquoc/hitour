f280d38f52cc3642543a1c76ebdeaaf0ceb88449
root <root@DESKTOP-UVJKVE7.localdomain>
2022-08-24 16:23:41 +0700

    change to noindex

--- resources/views/main/snippets/head.blade.php
+++ resources/views/main/snippets/head.blade.php
@@ -1,6 +1,6 @@
 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta name="robots" content="index,follow">
+<meta name="robots" content="noindex,nofollow">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta name="fragment" content="!"/>
 <title>Trang chủ Hitour.vn</title>
