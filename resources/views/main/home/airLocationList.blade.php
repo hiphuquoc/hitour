40eb175c07962c5d9f22cecdab955a795efd15d7
root <root@DESKTOP-UVJKVE7.localdomain>
2022-11-01 23:50:52 +0700

    --amend

--- resources/views/main/home/airLocationList.blade.php
+++ resources/views/main/home/airLocationList.blade.php
@@ -5,15 +5,6 @@
                 <a href="/{{ $airLocation->seo->slug_full ?? null }}" title="{{ $airLocation->name ?? $airLocation->seo->title ?? $airLocation->seo->seo_title ?? null }}" class="airLocationList_item_title">
                     <h3>{{ $airLocation->name ?? $airLocation->seo->title ?? null }}</h3>
                 </a>
-                {{-- <ul>
-                    @foreach($airLocation->airs as $air)
-                        <li>
-                            <a href="/{{ $air->seo->slug_full ?? null }}" title="{{ $air->name ?? $air->seo->slug_full ?? null }}">
-                                <h4>{{ $air->name ?? $air->seo->slug_full ?? null }}</h4>
-                            </a>
-                        </li>
-                    @endforeach
-                </ul> --}}
             </div>
         @endforeach
     </div>
