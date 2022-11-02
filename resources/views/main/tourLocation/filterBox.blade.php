aa133ae23f3549a8874a27c552751a32612b421b
root <root@DESKTOP-UVJKVE7.localdomain>
2022-11-02 11:44:57 +0700

    --amend

---
+++ resources/views/main/tourLocation/filterBox.blade.php
@@ -0,0 +1,26 @@
+<div class="hide-767">
+    <div class="filterBox">
+        <div class="filterBox_label">
+            Lọc theo:
+        </div>
+        <div class="filterBox_filter">
+            <div class="filterBox_filter_item active" onClick="filterTour(this, 'tat-ca-tour');">
+                <div>Tất cả</div>
+            </div>
+            <div class="filterBox_filter_item" onClick="filterTour(this, 'tour-tron-goi');">
+                <h3>Tour trọn gói</h3>
+            </div>
+            <div class="filterBox_filter_item" onClick="filterTour(this, 'tour-trong-ngay');">
+                <h3>Tour trong ngày</h3>
+            </div>
+        </div>
+        <div class="filterBox_view">
+            <div class="filterBox_view_item">
+                <i class="fa-solid fa-table-list"></i>
+            </div>
+            <div class="filterBox_view_item active">
+                <i class="fa-solid fa-table-cells"></i>
+            </div>
+        </div>
+    </div>
+</div>
