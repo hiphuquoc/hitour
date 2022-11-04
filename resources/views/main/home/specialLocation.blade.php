d00700215ac0b9d8279abf8b7bb48279b349e963
root <root@DESKTOP-UVJKVE7.localdomain>
2022-11-04 19:37:53 +0700

    fix box specialLocation

--- resources/views/main/home/specialLocation.blade.php
+++ resources/views/main/home/specialLocation.blade.php
@@ -3,18 +3,17 @@
         @foreach($specialLocations as $specialLocation)
             <div class="specialLocationBox_item">
                 <div class="specialLocationBox_item_box">
-                        <div class="specialLocationBox_item_box_image" style="background-image:url('{{ $specialLocation->seo->image ?? $specialLocation->seo->image_small ?? config('admin.images.default_750x460') }}');"></div>
-                        <a href="/{{ $specialLocation->seo->slug_full ?? null }}" title="{{ $specialLocation->name ?? $specialLocation->seo->title ?? $specialLocation->seo->seo_title ?? null }}">
-                            <div class="specialLocationBox_item_box_title">
-                                <h3 class="maxLine_1">{{ $specialLocation->display_name ?? null }}</h3>
-                            </div>
-                        </a>
-                        @php
-                            if($loop->index==12) break;
-                        @endphp
-                    
+                    <div class="specialLocationBox_item_box_image" style="background-image:url('{{ $specialLocation->seo->image ?? $specialLocation->seo->image_small ?? config('admin.images.default_750x460') }}');"></div>
+                    <a href="/{{ $specialLocation->seo->slug_full ?? null }}" title="{{ $specialLocation->name ?? $specialLocation->seo->title ?? $specialLocation->seo->seo_title ?? null }}">
+                        <div class="specialLocationBox_item_box_title">
+                            <h3 class="maxLine_1">{{ $specialLocation->display_name ?? null }}</h3>
+                        </div>
+                    </a>
                 </div>
             </div>
+            @php
+                if($loop->index==12) break;
+            @endphp
         @endforeach
     </div>
 @endif
