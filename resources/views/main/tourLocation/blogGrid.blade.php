05fea7a131acb008d51baf003f49402efa0050f1
root <root@DESKTOP-UVJKVE7.localdomain>
2022-11-08 18:00:30 +0700

    fix show blog in tour_location

--- resources/views/main/tourLocation/blogGrid.blade.php
+++ resources/views/main/tourLocation/blogGrid.blade.php
@@ -24,28 +24,6 @@
                         </div>
                     </div>
                 </div>
-                <div class="blogGrid_box_item">
-                    <div class="blogGrid_box_item_image">
-                        <a href="/{{ $blog->seo->slug_full ?? null }}" title="{{ $blog->name ?? $blog->seo->title ?? $blog->seo->seo_title ?? null }}">
-                            <img src="{{ config('main.svg.loading_main') }}" data-src="{{ $blog->seo->image_small ?? $blog->seo->image ?? config('admin.images.default_750x460') }}" alt="{{ $blog->name ?? $blog->seo->title ?? $blog->seo->seo_title ?? null }}" title="{{ $blog->name ?? $blog->seo->title ?? $blog->seo->seo_title ?? null }}" />
-                        </a>
-                    </div>
-                    <div class="blogGrid_box_item_content">
-                        <a href="/{{ $blog->seo->slug_full ?? null }}" title="{{ $blog->name ?? $blog->seo->title ?? $blog->seo->seo_title ?? null }}" class="blogGrid_box_item_content_title">
-                            <h3 class="maxLine_2" id="randomIdTocContent_53">
-                                {{ $blog->name ?? $blog->seo->title ?? null }}
-                            </h3>
-                        </a>
-                        @if(!empty($blog->seo->updated_at))
-                            <div class="blogGrid_box_item_content_time">
-                                <i class="fa-regular fa-calendar-days"></i>{{ date('H:i\, d/m/Y', strtotime($blog->seo->updated_at)) }}
-                            </div>
-                        @endif
-                        <div class="blogGrid_box_item_content_des maxLine_3">
-                            {{ $blog->description ?? $blog->seo->description ?? null }}
-                        </div>
-                    </div>
-                </div>
             @endforeach
         </div>
     </div>
