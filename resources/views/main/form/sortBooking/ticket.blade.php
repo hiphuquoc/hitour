6b3b5caff0990d222d17a9f7b3af9243a495dc6b
root <root@DESKTOP-UVJKVE7.localdomain>
2022-11-14 10:15:08 +0700

    update girignore don't follow cpanel.yml

--- resources/views/main/form/sortBooking/ticket.blade.php
+++ resources/views/main/form/sortBooking/ticket.blade.php
@@ -1,3 +1,3 @@
 {{-- <div class="bookFormSort_body_item"> --}}
-    <div>Form đặt Vé vui chơi đang bảo trì...! Để được hỗ trợ đặt vé nhanh nhất Quý khách vui lòng liên hệ <span style="font-size:1.2rem;color:rgb(0,123,255);font-weight:700;">08 6868 46868</span>.</div>
+    <div>Form đặt Vé vui chơi đang bảo trì...! Để được hỗ trợ đặt vé nhanh chóng Quý khách vui lòng liên hệ <span style="font-size:1.2rem;color:rgb(0,123,255);font-weight:700;">08 6868 46868</span>.</div>
 {{-- </div> --}}
