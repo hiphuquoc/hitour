5b1da56c3f845c606aae5aaa4bf81d9d176708bb
root <root@DESKTOP-UVJKVE7.localdomain>
2023-03-05 18:41:14 +0700

    --amend

--- resources/views/admin/shipBooking/viewExportHtml.blade.php
+++ resources/views/admin/shipBooking/viewExportHtml.blade.php
@@ -1,11 +1,12 @@
 @include('admin.shipBooking.confirmBooking', compact('item', 'infoStaff'))
 <script type="text/javascript">
     window.addEventListener("load", function() {
-        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-            /* nếu sử dụng điện thoại thì không tự gọi tính năng in */
-        } else {
-            window.print();
-        }
+        // if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        //     /* nếu sử dụng điện thoại thì không tự gọi tính năng in */
+        // } else {
+        //     window.print();
+        // }
+        window.print();
         /* sự kiện chuyển hướng sau khi in */
         window.addEventListener("afterprint", function(event) {
             window.location = document.referrer;
