c19f299cb9576d12479effe15248aa405fa1335d
root <root@DESKTOP-3JVPJ2G.localdomain>
2024-02-27 19:25:07 +0700

    code google ads

--- resources/views/admin/snippets/head.blade.php
+++ resources/views/admin/snippets/head.blade.php
@@ -58,4 +58,15 @@
   <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
   <!-- END: SLICK -->
 
+  <!-- Google tag (gtag.js) GOOGLE ADS -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16473804050">
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-16473804050');
+  </script>
+
  </head>
