60a0ae3506081c50c1d6f372fe62fdd8e885281c
root <root@DESKTOP-3JVPJ2G.localdomain>
2024-02-27 19:42:13 +0700

    --amend

--- resources/views/main/snippets/scripts-default.blade.php
+++ resources/views/main/snippets/scripts-default.blade.php
@@ -2,13 +2,14 @@
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <!-- END: SLICK -->
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-G9LSM1829M"></script>
+<!-- Google tag (gtag.js) GOOGLE ADS -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16473804050"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'G-G9LSM1829M');
+
+  gtag('config', 'AW-16473804050');
 </script>
 
 <script type="text/javascript">
